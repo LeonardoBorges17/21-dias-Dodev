78338ed3916be2b27c950b2399e58390cbedb38d
Leonardo Borges <leonardo.appelt.boegs@gmail.com>
2024-11-10 23:46:29 -0300

    adicionando exercicio da aula do dia 13

---
+++ dia 13/ex1.js
@@ -0,0 +1,30 @@
+class Computador {
+    Tipo
+    Processador
+    Video
+    Armazenamento
+    MemoriaRam
+    Ssd
+
+    constructor(tipo, processador, video, armazenamento, memoriaRam, ssd) {
+        this.Tipo = tipo
+        this.Processador = processador
+        this.Video = video
+        this.Armazenamento = armazenamento
+        this.MemoriaRam = memoriaRam
+        this.Ssd = ssd
+    }
+
+    ExibirInformacoes() {
+        console.log("Informações do Computador:")
+        console.log(`Tipo: ${this.Tipo}`)
+        console.log(`Processador: ${this.Processador}`)
+        console.log(`Video: ${this.Video}`)
+        console.log(`Armazenamento: ${this.Armazenamento}`)
+        console.log(`Memória RAM: ${this.MemoriaRam}`)
+        console.log(`SSD: ${this.Ssd}`)
+    }
+}
+
+const meuComputador = new Computador("notebook", "ryzen 7", "dedicado", 1000, 16, true)
+meuComputador.ExibirInformacoes()
