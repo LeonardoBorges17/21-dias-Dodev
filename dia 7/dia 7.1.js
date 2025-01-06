5b7c9f036ae0cf561d93ab07d07c4433f36cd23c
Leonardo Borges <leonardo.appelt.boegs@gmail.com>
2025-01-05 23:56:11 -0300

    adicionando exercicio do dia 7

---
+++ dia 7/dia 7.1.js
@@ -0,0 +1,51 @@
+let saldo = 1000
+let maiorValorInserido = 0
+let somaValoresInseridos = 0
+let totalTransacoes = 0
+let continuar = false
+
+do {
+    const nome = prompt("Digite o nome do seu cliente:")
+    const cpf = prompt("digite o cpf do seu cliente")   
+    const valor = parseInt(prompt("Qual o valor que será movimentado?"))
+    const operacao = prompt("Qual operação deseja fazer?     S / D")
+
+    if (valor < 0) {
+        console.log("Valor inválido. A transação não foi realizada.")
+    } else if (operacao == "S" && valor > saldo) {
+        console.log("Saldo insuficiente. A transação não foi realizada")
+    } else if (operacao == "S") {
+        console.log(`Olá, ${nome} (${cpf}), seu saldo atual é R$${saldo}.`)
+        saldo -= valor
+        totalTransacoes++
+        somaValoresInseridos += valor
+        if (valor > maiorValorInserido) {
+            maiorValorInserido = valor
+        }
+        console.log(`Transação realizada com sucesso. Seu saldo atual é R$${saldo}.`)
+    } else {
+        console.log(`Olá, ${nome} (${cpf}), seu saldo atual é R$${saldo}.`)
+        saldo += valor
+        totalTransacoes++
+        somaValoresInseridos += valor
+        if (valor > maiorValorInserido) {
+            maiorValorInserido = valor
+        }
+        console.log(`Transação realizada com sucesso. Seu saldo atual é R$${saldo}.`)
+    }
+
+    const opcao = prompt("Deseja continuar? (1 para continuar, 2 para parar):")
+    if (opcao === "1") {
+        continuar = true
+    } else if (opcao === "2") {
+        continuar = false
+    } else {
+        console.log("Opção inválida. Programa encerrado")
+        continuar = false
+    }
+
+} while (continuar)
+
+console.log(`Saldo final: R$${saldo}`)
+console.log(`Maior valor inserido: R$${maiorValorInserido}`)
+console.log(`Média dos valores inseridos: R$${(somaValoresInseridos / totalTransacoes)}`)
